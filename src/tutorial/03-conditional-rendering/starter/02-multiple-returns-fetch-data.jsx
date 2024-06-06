0a2c55112a2d4ea6034485a5e410151507fb1129
dollarp-code <husseindollarp@yahoo.com>
2024-06-06 18:06:08 +0100

    Understanding multiple returns fetch data

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,43 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const result = await fetch(url);
+        const user = await result.json();
+        // console.log(user);
+        setUser(user);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+
+  return (
+    <div>
+      <img
+        src={user.avatar_url}
+        alt={user.name}
+        style={{ width: '150px', borderRadius: '10px' }}
+      />
+      <h2>{user.name}</h2>
+      <h4>{user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
