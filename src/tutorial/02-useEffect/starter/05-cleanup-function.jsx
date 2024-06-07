23ac57b83db60ac233ae828fae37e20c396919a6
dollarp-code <husseindollarp@yahoo.com>
2024-06-07 12:53:03 +0100

    Understanding use effect and conditional rendering

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      <button className="btn" type="button" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <AnyComponent />}
+    </div>
+  );
+};
+
+const AnyComponent = () => {
+  useEffect(() => {
+    const someFunc = () => {
+      // some logic
+    };
+    window.addEventListener('scroll', someFunc);
+    return () => window.removeEventListener('scroll', someFunc);
+  }, []);
+  return <h2>Hello there</h2>;
 };
 
 export default CleanupFunction;
